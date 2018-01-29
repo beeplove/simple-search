90839f754f1b0baa50a40d0fe0e8a58669f5873c
Mohammad Khan <mohammad.khan@gmx.us>
2018-01-28 21:32:11 -0800

    added barebone Entity as a representation of the data

---
+++ spec/lib/entity/entity_spec.rb
@@ -0,0 +1,14 @@
+RSpec.describe Entity do
+
+  describe "#list" do
+    it "returns list of entities avaiable"
+  end
+
+  describe "#fields" do
+    it "returns list of fields avaiable for a given entity name"
+  end
+
+  describe "#data" do
+    it "returns data avaiable for a given entity name"
+  end
+end
