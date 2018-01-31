60252bffdbabbdd3365ed62467e2e673e7a2f82f
Mohammad Khan <mohammad.khan@gmx.us>
2018-01-31 09:38:28 -0800

    display entities from mock data in search form

--- web-ui/src/app/search/search.component.ts
+++ web-ui/src/app/search/search.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Entity } from '../entity';
+
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent implements OnInit {
+  entityId: string = '';
+  entities: Entity[] = [];
 
   constructor() { }
 
   ngOnInit() {
+    this.getEntities()
   }
 
+  getEntities(): void {
+    this.entities.push(new Entity(0, "Any"))
+    this.entities.push(new Entity(1, "organizations"))
+    this.entities.push(new Entity(2, "users"))
+    this.entities.push(new Entity(3, "tickets"))
+  }
 }
